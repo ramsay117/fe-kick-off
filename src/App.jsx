bab93507ed394cac78a1cb880da75ee1ceb5c8c4
Shubham Gupta <shubham@betaflux.co>
2023-03-29 21:11:20 +0530

    vite-setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div>App</div>;
+}
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
